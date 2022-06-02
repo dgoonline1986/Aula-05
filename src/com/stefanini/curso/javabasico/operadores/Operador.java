f7c019a15e56b1e7b74bc7c0771f1eebb102b8ff
Diego Nunes <dgoonline1986@gmail.com>
2022-06-02 12:36:22 -0300

    first commit

---
+++ src/com/stefanini/curso/javabasico/operadores/Operador.java
@@ -0,0 +1,15 @@
+package com.stefanini.curso.javabasico.operadores;
+
+public class Operador {
+
+	public static void main(String[] args) {
+		
+		int salario= 3000;
+		
+		System.out.println(salario>3000?"Senior":"Analista Junior");
+		
+		}
+
+	}
+
+
